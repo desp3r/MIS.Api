67acdc74d87548c4930ddc3fc4b12048375901df
Dima <dima@mydomain.com>
2023-03-22 17:40:49 +0200

    manual revert + some tests

--- MIS.IntegrationTests/EmployeeControllerTests.cs
+++ MIS.IntegrationTests/EmployeeControllerTests.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using MIS.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using MIS.Business.Extensions;
+using System.Net.Http.Json;
 
 namespace MIS.IntegrationTests
 {
@@ -102,27 +103,18 @@ namespace MIS.IntegrationTests
             //Arrange
             HttpClient client = GetWebApplication().CreateClient();
             string route = ApiRoutes.Employee.CRUD;
-            var reg = new RandomEntityGenerator();
 
-            var oldEmployee = await TestRepository.FirstOrDefaultAsync<Employee>(e => true);
-            if (oldEmployee == default)
+            var employee = await TestRepository.FirstOrDefaultAsync<Employee>(e => true);
+            if (employee == default)
             {
-                await TestRepository.CreateAsync(reg.Employee);
+                await TestRepository.CreateAsync(new RandomEntityGenerator().Employee);
                 await TestRepository.SaveChangesAsync();
-                oldEmployee = await TestRepository.SingleAsync<Employee>(e => true);
+                employee = await TestRepository.SingleAsync<Employee>(e => true);
             }
 
-            var newEmployee = reg.Employee;
             var request = new EmployeeModel()
             {
-                Id = oldEmployee.Id,
-                UserId = newEmployee.UserId,
-                OrganizationId = newEmployee.OrganizationId,
-                FirstName = newEmployee.FirstName,
-                LastName = newEmployee.LastName,
-                MiddleName = newEmployee.MiddleName,
-                EmployeeType = newEmployee.EmployeeType,
-                WorkingExpYears = newEmployee.WorkingExpYears,
+                Id = employee.Id
             };
 
             //Act
@@ -131,14 +123,59 @@ namespace MIS.IntegrationTests
             //Assert
             responce.StatusCode.Should().Be(HttpStatusCode.OK);
             (await TestRepository.FirstOrDefaultAsync<Employee>(e =>
-                e.Id == oldEmployee.Id &&
-                e.UserId == request.UserId &&
-                e.OrganizationId == request.OrganizationId &&
-                e.FirstName == request.FirstName &&
-                e.MiddleName == request.MiddleName &&
-                e.LastName == request.LastName &&
-                e.EmployeeType == request.EmployeeType &&
-                e.WorkingExpYears == request.WorkingExpYears)).Should().NotBeNull();
+                e.Id == employee.Id)).Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Get_Should_ReturnAnEmployee()
+        {
+            //Arrange
+            HttpClient client = GetWebApplication().CreateClient();
+            string route = ApiRoutes.Employee.CRUD;
+
+            var employee = await TestRepository.FirstOrDefaultAsync<Employee>(e => true);
+            if (employee == default)
+            {
+                await TestRepository.CreateAsync(new RandomEntityGenerator().Employee);
+                await TestRepository.SaveChangesAsync();
+                employee = await TestRepository.SingleAsync<Employee>(e => true);
+            }
+
+            var request = new EmployeeModel()
+            {
+                Id = employee.Id
+            };
+
+            //Act
+            var responce = await client.GetAsync(route + "/" + employee.Id);
+
+            //Assert
+            responce.StatusCode.Should().Be(HttpStatusCode.OK);
+            var result = await responce.Content.ReadAsAsync<Employee>();
+            result.Equals(employee).Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task List_Should_ReturnAllEmployees()
+        {
+            //Arrange
+            HttpClient client = GetWebApplication().CreateClient();
+            string route = ApiRoutes.Employee.List;
+            var reg = new RandomEntityGenerator();
+
+            await TestRepository.CreateAsync(reg.Employee);//add some employees
+            await TestRepository.CreateAsync(reg.Employee);
+            await TestRepository.CreateAsync(reg.Employee);
+            await TestRepository.SaveChangesAsync();
+
+            var employees = await TestRepository.GetAllAsync<Employee>();
+
+            //Act
+            var responce = await client.GetAsync(route);
+
+            //Assert
+            responce.StatusCode.Should().Be(HttpStatusCode.OK);
+            var result = await responce.Content.ReadAsAsync<IEnumerable<Employee>>();
         }
     }
 }
